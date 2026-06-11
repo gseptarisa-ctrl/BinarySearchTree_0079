ef23e70ca5c9e61d30ace3a19855102144acbde1
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-06-11 17:57:44 +0700

    membuat fungsi search untuk mencari data

--- searchTree.cpp
+++ searchTree.cpp
@@ -56,3 +56,8 @@ public:
         }
     }
 
+    //this function searches the current node of the specified node
+    //as well as the current node of its parent
+    void search(string element, Node* &parent, Node* &currentNode) 
+    {
+        
